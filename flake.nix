d4e32573475e2a309e66ae23095210ba4c1115d4
Daher12 <133640261+Daher12@users.noreply.github.com>
2026-01-16 14:11:15 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,127 +1,69 @@
 {
-  description = "Unified NixOS Configuration";
+  description = "NixOS Configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-
-    lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.4.3";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     lix-module = {
-      url = "git+https://git.lix.systems/lix-project/nixos-module?ref=release-2.93";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.0.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    winapps = {
-      url = "github:winapps-org/winapps/44342c34b839547be0b2ea4f94ed00293fa7cc38";
+    lanzaboote = {
+      url = "github:nix-community/lanzaboote/v0.3.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    preload-ng = {
-      url = "github:miguel-b-p/preload-ng/eb3c66a20d089ab2e3b8ff34c45c3d527584ed38";
+    # NEW: SOPS-Nix for secret management
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs =
-    inputs@{ self, nixpkgs, ... }:
+    {
+      self,
+      nixpkgs,
+      lix-module,
+      home-manager,
+      lanzaboote,
+      sops-nix, # Added to args
+      ...
+    }@inputs:
     let
-      forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" ];
-
-      pkgsFor =
-        system:
-        import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
-
-      palette = import ./lib/palette.nix;
-
-      overlays = system: [
-        (_final: _prev: {
-          unstable = import inputs.nixpkgs-unstable {
-            inherit system;
-            config.allowUnfree = true;
-          };
-        })
-      ];
-
       mkHost = import ./lib/mkHost.nix {
         inherit
-          nixpkgs
           inputs
+          nixpkgs
           self
-          palette
-          overlays
           ;
+        palette = import ./lib/palette.nix;
+        overlays = import ./overlays { inherit inputs; };
       };
     in
     {
-      formatter = forAllSystems (system: (pkgsFor system).nixfmt-rfc-style);
-
-      checks = forAllSystems (
-        system:
-        let
-          pkgs = pkgsFor system;
-        in
-        {
-          statix = pkgs.runCommand "statix-check" { buildInputs = [ pkgs.statix ]; } ''
-            statix check ${self} && touch $out
-          '';
-          deadnix = pkgs.runCommand "deadnix-check" { buildInputs = [ pkgs.deadnix ]; } ''
-            deadnix --fail ${self} && touch $out
-          '';
-          nixfmt = pkgs.runCommand "nixfmt-check" { buildInputs = [ pkgs.nixfmt-rfc-style ]; } ''
-            find ${self} -name '*.nix' -exec nixfmt --check {} + && touch $out
-          '';
-        }
-      );
-
       nixosConfigurations = {
         yoga = mkHost {
-          system = "x86_64-linux";
           hostname = "yoga";
-          mainUser = "dk";
-          profiles = [
-            "laptop"
-            "desktop-gnome"
-          ];
-          extraModules = [
-            inputs.nixos-hardware.nixosModules.lenovo-yoga-7-slim-gen8
-            ./hosts/yoga/default.nix
-          ];
+          mainUser = "david";
+          profiles = [ "laptop" ];
           hmModules = [ ./hosts/yoga/home.nix ];
-          extraSpecialArgs = {
-            winappsPackages = inputs.winapps.packages."x86_64-linux";
-          };
         };
 
-        e7450-nixos = mkHost {
-          system = "x86_64-linux";
-          hostname = "e7450-nixos";
-          mainUser = "dk";
+        latitude = mkHost {
+          hostname = "latitude";
+          mainUser = "david";
           profiles = [
             "laptop"
             "desktop-gnome"
           ];
-          extraModules = [
-            inputs.preload-ng.nixosModules.default
-            ./hosts/latitude/default.nix
-          ];
           hmModules = [ ./hosts/latitude/home.nix ];
-          extraSpecialArgs = {
-            winappsPackages = null;
-          };
         };
       };
     };
