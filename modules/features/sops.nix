65755e0136e169d7c46bcb32401e88fae5fe08b0
Daher12 <133640261+Daher12@users.noreply.github.com>
2026-01-16 23:44:44 +0100

    Update sops.nix

--- modules/features/sops.nix
+++ modules/features/sops.nix
@@ -1,20 +1,16 @@
-{ inputs, config, lib, flakeRoot, ... }:
+{ config, lib, flakeRoot, ... }:
 
 let
   cfg = config.features.sops;
   hostname = config.networking.hostName;
-  # Use the passed-in flakeRoot (guaranteed to be a Store Path)
   secretsFile = flakeRoot + "/secrets/hosts/${hostname}.yaml";
 in
 {
-  imports = [ inputs.sops-nix.nixosModules.sops ];
-
   options.features.sops = {
     enable = lib.mkEnableOption "SOPS Secret Management";
   };
 
   config = lib.mkIf cfg.enable {
-    # Fail fast if the secrets file is missing from the flake source
     assertions = [
       {
         assertion = builtins.pathExists secretsFile;
@@ -25,9 +21,6 @@ in
     sops = {
       defaultSopsFormat = "yaml";
       defaultSopsFile = secretsFile;
-
-      # Persistent key storage on the target host
-      # Use a string here because it refers to a path on the DEPLOYED system, not the build host.
       age.keyFile = "/var/lib/sops-nix/key.txt";
     };
   };
