12fabfdbeefd4db5229d4fb2994c8288b909de28
Daher12 <133640261+Daher12@users.noreply.github.com>
2026-01-16 11:44:20 +0100

    Update git.nix

--- home/git.nix
+++ home/git.nix
@@ -1,95 +1,48 @@
 {
   config,
-  lib,
   pkgs,
   ...
 }:
 
-let
-  cfg = config.programs.git;
-in
 {
-  # Namespace changed from features.git to programs.git
-  options.programs.git = {
-    identity = {
-      name = lib.mkOption {
-        type = lib.types.str;
-        default = "Daher12";
-        description = "Git user name";
+  # Directly configure the upstream module. 
+  # No custom options needed.
+  programs.git = {
+    enable = true;
+    package = pkgs.gitMinimal;
+    
+    # Set defaults here. If you need to override them per-host, 
+    # you can just set 'programs.git.userName = "..."' in that host's home.nix.
+    userName = "Daher12";
+    userEmail = "133640261+Daher12@users.noreply.github.com";
+
+    extraConfig = {
+      init.defaultBranch = "main";
+      core.editor = "ox";
+
+      pull.rebase = true;
+      rebase.autoStash = true;
+      fetch.prune = true;
+      push.autoSetupRemote = true;
+
+      "url \"ssh://git@github.com/\"".insteadOf = "https://github.com/";
+
+      diff = {
+        colorMoved = "default";
+        algorithm = "histogram";
       };
-
-      email = lib.mkOption {
-        type = lib.types.str;
-        default = "133640261+Daher12@users.noreply.github.com";
-        description = "Git user email";
-      };
-    };
-
-    # This extends the standard programs.git.delta options if they exist,
-    # or defines them if they don't.
-    delta = {
-      enable = lib.mkOption {
-        type = lib.types.bool;
-        default = true;
-        description = "Enable delta diff viewer";
-      };
-
-      sideBySide = lib.mkOption {
-        type = lib.types.bool;
-        default = false;
-        description = "Use side-by-side diff view";
-      };
-    };
-
-    defaultBranch = lib.mkOption {
-      type = lib.types.str;
-      default = "main";
-      description = "Default branch name for new repositories";
-    };
-
-    editor = lib.mkOption {
-      type = lib.types.str;
-      default = "ox";
-      description = "Text editor for git operations";
+      merge.conflictStyle = "zdiff3";
     };
   };
 
-  config = {
-    programs.delta = {
-      inherit (cfg.delta) enable;
-      options = {
-        navigate = true;
-        line-numbers = true;
-        hyperlinks = true;
-        side-by-side = cfg.delta.sideBySide;
-      };
-    };
-
-    programs.git = {
-      enable = true;
-      package = pkgs.gitMinimal;
-      
-      # Use the extended options defined above
-      userName = cfg.identity.name;
-      userEmail = cfg.identity.email;
-
-      extraConfig = {
-        init.defaultBranch = cfg.defaultBranch;
-        core.editor = cfg.editor;
-
-        pull.rebase = true;
-        rebase.autoStash = true;
-        fetch.prune = true;
-        push.autoSetupRemote = true;
-
-        "url \"ssh://git@github.com/\"".insteadOf = "https://github.com/";
-        
-        diff = {
-          colorMoved = "default";
-          algorithm = "histogram";
-        };
-        merge.conflictStyle = "zdiff3";
-      };
+  # Configure Delta directly using its own module
+  programs.delta = {
+    enable = true;
+    options = {
+      navigate = true;
+      line-numbers = true;
+      hyperlinks = true;
+      # side-by-side = false; # Uncomment/Change here if needed
     };
   };
 }
