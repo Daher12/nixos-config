874171586e9035626ce862a834682139216e21bc
Daher12 <133640261+Daher12@users.noreply.github.com>
2026-01-23 13:00:00 +0100

    Update sysctl.nix

--- modules/core/sysctl.nix
+++ modules/core/sysctl.nix
@@ -10,14 +10,14 @@ in
 
   config = lib.mkIf cfg.optimizeForServer {
     boot.kernel.sysctl = {
-      "vm.swappiness" = lib.mkDefault 10;
-      "vm.vfs_cache_pressure" = lib.mkDefault 50;
-      "vm.dirty_background_bytes" = lib.mkDefault 134217728;
-      "vm.dirty_bytes" = lib.mkDefault 536870912;
-      "fs.inotify.max_user_watches" = lib.mkDefault 1048576;
-      "fs.inotify.max_user_instances" = lib.mkDefault 1024;
-      "net.core.somaxconn" = lib.mkDefault 4096;
-      "net.ipv4.ip_local_port_range" = lib.mkDefault "10240 65535";
+      "vm.swappiness" =  10;
+      "vm.vfs_cache_pressure" =  50;
+      "vm.dirty_background_bytes" =  134217728;
+      "vm.dirty_bytes" =  536870912;
+      "fs.inotify.max_user_watches" =  1048576;
+      "fs.inotify.max_user_instances" =  1024;
+      "net.core.somaxconn" =  4096;
+      "net.ipv4.ip_local_port_range" =  "10240 65535";
     };
   };
 }
