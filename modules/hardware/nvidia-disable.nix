6fe65ec9afbfa9ec42fa8df601ed490677bf3d8b
Daher12 <133640261+Daher12@users.noreply.github.com>
2026-01-26 14:41:52 +0100

    Update nvidia-disable.nix

--- modules/hardware/nvidia-disable.nix
+++ modules/hardware/nvidia-disable.nix
@@ -1,57 +1,43 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 let
   cfg = config.hardware.nvidia.disable;
-
-  # When set, only these PCI functions are removed (safer than vendor-wide match).
-  # Example BDF: "0000:01:00.0" (include all functions e.g. .0/.1/.2 if present).
-  udevRemoveRules =
-    if cfg.pciAddresses != [] then
-      lib.concatMapStringsSep "\n" (addr: ''
-        ACTION=="add", SUBSYSTEM=="pci", KERNEL=="${addr}", ATTR{power/control}="auto", ATTR{remove}="1"
-      '') cfg.pciAddresses
-    else
-      ''
-        # Default (broad): remove NVIDIA GPU + common companion functions by class.
-        # udev uses glob-style matching; keep patterns simple/predictable.
-        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1" # xHCI
-        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1" # UCSI/other
-        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1" # HDA audio
-        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03*",     ATTR{power/control}="auto", ATTR{remove}="1" # VGA/3D
-      '';
 in
 {
   options.hardware.nvidia.disable = {
-    enable = lib.mkEnableOption "NVIDIA dGPU removal via udev (aim: 0W draw)";
-
-    pciAddresses = lib.mkOption {
-      type = lib.types.listOf lib.types.str;
-      default = [];
-      example = [ "0000:01:00.0" "0000:01:00.1" "0000:01:00.2" ];
-      description = "Optional allowlist of PCI BDFs to remove; when empty, uses broad vendor/class matching.";
-    };
+    enable = lib.mkEnableOption "completely disable Nvidia GPU";
   };
 
   config = lib.mkIf cfg.enable {
-    # 1) Block kernel drivers from auto-loading
-    boot.blacklistedKernelModules = [
-      "nouveau"
-      "nvidia"
-      "nvidia_drm"
-      "nvidia_modeset"
-    ];
+    # FIX: Grouped "boot" attributes to satisfy Statix
+    boot = {
+      blacklistedKernelModules = [
+        "nouveau"
+        "nvidia"
+        "nvidia_drm"
+        "nvidia_modeset"
+      ];
 
-    # Make early-boot behavior predictable (initrd/module autoload).
-    boot.kernelParams = [
-      "module_blacklist=nouveau,nvidia,nvidia_drm,nvidia_modeset"
-    ];
+      kernelParams = [
+        "nouveau.modeset=0"
+        "rd.driver.blacklist=nouveau"
+      ];
 
-    boot.extraModprobeConfig = ''
-      blacklist nouveau
-      options nouveau modeset=0
+      extraModprobeConfig = ''
+        blacklist nouveau
+        options nouveau modeset=0
+      '';
+    };
+
+    services.udev.extraRules = ''
+      # Remove NVIDIA USB xHCI Host Controller devices, if present
+      ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
+      # Remove NVIDIA USB Type-C UCSI devices, if present
+      ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
+      # Remove NVIDIA Audio devices, if present
+      ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+      # Remove NVIDIA VGA/3D controller devices
+      ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
     '';
-
-    # 2) Udev “remove” rules
-    services.udev.extraRules = udevRemoveRules;
   };
 }
