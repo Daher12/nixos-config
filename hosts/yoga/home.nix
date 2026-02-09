f4a061cf192d55b652c60693bc12712559e02f3d
Daher12 <133640261+Daher12@users.noreply.github.com>
2026-02-09 11:33:04 +0100

    Configure home persistence for user directories
    Add home persistence configuration for user directories.

--- hosts/yoga/home.nix
+++ hosts/yoga/home.nix
@@ -1,16 +1,24 @@
 { ... }:
-
 {
-  imports = [
-    ../../home
-  ];
+  imports = [ ../../home ];
 
   home.stateVersion = "25.11";
 
   programs.winapps.enable = false;
-
   browsers = {
     firefox.enable = true;
     brave.enable = true;
+  };
+
+  home.persistence."/persist/home/dk" = {
+    directories = [
+      "Documents"
+      "Downloads"
+      ".ssh"
+      ".gnupg"
+      ".mozilla"
+      ".local/share/keyrings"
+    ];
+    allowOther = true;
   };
 }
