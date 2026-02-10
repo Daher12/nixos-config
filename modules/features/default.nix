db50a3a9eb57b000a2b27072f70d36cb7dc8c6b6
Daher12 <133640261+Daher12@users.noreply.github.com>
2026-02-10 14:56:08 +0100

    Update default.nix

--- modules/features/default.nix
+++ modules/features/default.nix
@@ -4,11 +4,13 @@
     ./desktop-gnome.nix
     ./filesystem.nix
     ./fonts.nix
+    ./impermanence.nix
     ./kernel.nix
     ./nas.nix
     ./network-optimization.nix
     ./oomd.nix
     ./power-tlp.nix
+    ./secureboot.nix 
     ./sops.nix
     ./virtualization.nix
     ./vpn.nix
