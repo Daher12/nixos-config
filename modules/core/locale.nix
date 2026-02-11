247be8ccb4ef147f3e3d0200e7357c6567796301
Daher12 <133640261+Daher12@users.noreply.github.com>
2026-02-11 12:49:41 +0100

    Update locale.nix

--- modules/core/locale.nix
+++ modules/core/locale.nix
@@ -1,46 +1,12 @@
+{ pkgs, lib, ... }:
 {
-  config,
-  lib,
-  pkgs,
-  ...
-}:
+  time.timeZone = lib.mkDefault "Europe/Berlin";
+  i18n.defaultLocale = lib.mkDefault "de_DE.UTF-8";
 
-let
-  cfg = config.core.locale;
-in
-{
-  options.core.locale = {
-    timeZone = lib.mkOption {
-      type = lib.types.str;
-      description = "System timezone";
-    };
-
-    defaultLocale = lib.mkOption {
-      type = lib.types.str;
-      description = "System locale";
-    };
-
-    console = {
-      earlySetup = lib.mkOption {
-        type = lib.types.bool;
-        default = true;
-        description = "Setup console early in boot";
-      };
-      font = lib.mkOption {
-        type = lib.types.str;
-        default = "ter-v16n";
-        description = "Console font";
-      };
-    };
-  };
-
-  config = {
-    time.timeZone = cfg.timeZone;
-    i18n.defaultLocale = cfg.defaultLocale;
-    console = {
-      inherit (cfg.console) earlySetup font;
-      packages = [ pkgs.terminus_font ];
-      useXkbConfig = true;
-    };
+  console = {
+    earlySetup = true;
+    font = "ter-v16n";
+    packages = [ pkgs.terminus_font ];
+    useXkbConfig = true;
   };
 }
