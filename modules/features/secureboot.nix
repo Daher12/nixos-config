87d33f72576eda88f78d071b896ae384a0131db7
Daher12 <133640261+Daher12@users.noreply.github.com>
2026-03-01 12:39:20 +0100

    secureboot.nix aktualisieren

--- modules/features/secureboot.nix
+++ modules/features/secureboot.nix
@@ -11,28 +11,32 @@ let
 in
 {
   imports = [ inputs.lanzaboote.nixosModules.lanzaboote ];
+
   options.features.secureboot = {
     enable = lib.mkEnableOption "Lanzaboote Secure Boot support";
+
     pkiBundle = lib.mkOption {
       type = lib.types.str;
       default = "/var/lib/sbctl";
     };
   };
-  config = lib.mkIf cfg.enable (
-    lib.mkMerge [
-      {
-        boot.loader.systemd-boot.enable = lib.mkForce false;
-        boot.lanzaboote = {
-          enable = true;
-          inherit (cfg) pkiBundle;
-        };
-        environment.systemPackages = [ pkgs.sbctl ];
-      }
 
-      # Lazy evaluation: only activates when impermanence is enabled
-      (lib.mkIf (config.features.impermanence.enable or false) {
-        environment.persistence."/persist/system".directories = [ cfg.pkiBundle ];
-      })
-    ]
-  );
+  config = lib.mkMerge [
+    (lib.mkIf (config.features.impermanence.enable or false) {
+      environment.persistence."/persist/system".directories = [ cfg.pkiBundle ];
+    })
+
+    (lib.mkIf ((config.features.impermanence.enable or false) || cfg.enable) {
+      environment.systemPackages = [ pkgs.sbctl ];
+    })
+
+    (lib.mkIf cfg.enable {
+      boot.loader.systemd-boot.enable = lib.mkForce false;
+
+      boot.lanzaboote = {
+        enable = true;
+        inherit (cfg) pkiBundle;
+      };
+    })
+  ];
 }
