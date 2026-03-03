9ab8c887986a5ac93406b063ee30b08329375dc0
Daher12 <133640261+Daher12@users.noreply.github.com>
2026-03-04 00:50:46 +0100

    home: persist fish config directory instead of fish_variables file

--- hosts/yoga/home.nix
+++ hosts/yoga/home.nix
@@ -10,30 +10,33 @@ _: {
     brave.enable = true;
   };
 
- home.persistence."/persist" = {
-  directories = [
-    {
-      directory = ".ssh";
-      mode = "0700";
-    }
-    {
-      directory = ".gnupg";
-      mode = "0700";
-    }
-    {
-      directory = ".config/sops/age";
-      mode = "0700";
-    }
+  home.persistence."/persist" = {
+    # Only app-state dotfiles remain here; XDG dirs moved to system
+    # persistence for hideMounts + allowTrash support.
+    directories = [
+      {
+        directory = ".ssh";
+        mode = "0700";
+      }
+      {
+        directory = ".gnupg";
+        mode = "0700";
+      }
+      {
+        directory = ".config/sops/age";
+        mode = "0700";
+      }
+      {
+        directory = ".config/fish";
+        mode = "0700";
+      }
+      ".local/share/keyrings"
+      ".mozilla/firefox"
+      ".config/BraveSoftware/Brave-Browser/Default"
+      ".local/state/wireplumber"
+      "nixos-config"
+    ];
 
-    ".local/share/keyrings"
-    ".mozilla/firefox"
-    ".config/BraveSoftware/Brave-Browser/Default"
-    ".local/state/wireplumber"
-    "nixos-config"
-  ];
-
-  files = [
-    ".config/fish/fish_variables"
-  ];
- };
+    files = [ ];
+  };
 }
