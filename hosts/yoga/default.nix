5273eef85083393897a06628a6137ef237fc065a
Daher12 <133640261+Daher12@users.noreply.github.com>
2026-08-10 23:29:02 +0200

    yoga: use powersave governor on AC, performance platform profile

--- hosts/yoga/default.nix
+++ hosts/yoga/default.nix
@@ -158,13 +158,13 @@
       TLP_DEFAULT_MODE = "BAT";
       CPU_DRIVER_OPMODE_ON_AC = "active";
       CPU_DRIVER_OPMODE_ON_BAT = "active";
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_AC = "powersave";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
       CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
       CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
       CPU_SCALING_MIN_FREQ_ON_AC = 403730;
       CPU_SCALING_MIN_FREQ_ON_BAT = 403730;
-      PLATFORM_PROFILE_ON_AC = "balanced";
+      PLATFORM_PROFILE_ON_AC = "performance";
       PLATFORM_PROFILE_ON_BAT = "balanced";
       PCIE_ASPM_ON_BAT = "powersupersave";
     };
